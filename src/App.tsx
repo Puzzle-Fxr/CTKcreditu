f03368e3cf6b29ebbc2d1277f45ecb289712179b
Puzzle-Fxr <caldwellavogo@gmail.com>
2026-06-22 22:03:41 +0000

    added scroll to top on pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './pages/ScrollToTop';
 import Home from './pages/Home';
 import TermsOfService from './pages/TermsOfService';
 
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/terms-of-service" element={<TermsOfService />} />
